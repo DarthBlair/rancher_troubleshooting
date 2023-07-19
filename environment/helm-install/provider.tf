1ed10dc07004f7f659bc33df14b558cd9b11efe5
marcel <m@bassueners.de>
2023-07-19 16:07:00 +0200

    beginn helm rancher branch

---
+++ environment/helm-install/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.10.1"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+
+}
