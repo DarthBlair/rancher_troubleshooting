39ac0d71338de14f5c15f48a7cb7ae5d14664b17
marcel <m@bassueners.de>
2023-07-20 08:27:15 +0200

    cert manager + rancher , crd not yet working

--- environment/helm-install/main.tf
+++ environment/helm-install/main.tf
@@ -1,26 +1,96 @@
-resource "helm_release" "example" {
-  name       = "rancher-stable"
-  repository = "https://releases.rancher.com/server-charts/stable"
-  chart      = "redis"
-  version    = "6.0.1"
+# resource "null_resource" "cert-manager-crds" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+# kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/v${var.certmanager_version}/deploy/manifests/00-crds.yaml
+# kubectl create namespace cert-manager
+# kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+# EOF
 
-  values = [
-    "${file("values.yaml")}"
-  ]
+
+#     environment = {
+#       KUBECONFIG = local_file.kube_cluster_yaml.filename
+#     }
+#   }
+# }
+
+#kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.12.2/cert-manager.crds.yaml
+data "http" "manifestfile" {
+  url = "https://github.com/cert-manager/cert-manager/releases/download/v1.12.2/cert-manager.crds.yaml"
+}
+
+
+resource "kubectl_manifest" "cert-manager-crds" {
+    yaml_body = data.http.manifestfile.response_body
+}
+
+
+
+
+# install cert-manager
+resource "helm_release" "cert_manager" {
+  depends_on = [kubectl_manifest.cert-manager-crds]
+  repository = "https://charts.jetstack.io"
+  version    = "v1.12.2"
+  name       = "cert-manager"
+  chart      = "cert-manager"
+  namespace  = "cert-manager"
+  create_namespace = true
+
+
+  # Bogus set to link together resources for proper tear down
+}
+
+# helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname=rancher.rd.localhost --wait --timeout=10m
+# install rancher
+resource "helm_release" "rancher" {
+  name      = "rancher"
+  repository ="https://releases.rancher.com/server-charts/stable"
+  chart     = "rancher"
+  version   = "v2.6.12"
+  namespace = "cattle-system"
+  create_namespace = true
+  timeout = "600"
 
   set {
-    name  = "cluster.enabled"
-    value = "true"
+    name  = "hostname"
+    value = "rancher.rd.localhost"
   }
 
   set {
-    name  = "metrics.enabled"
-    value = "true"
+    name  = "bootstrapPassword"
+    value = "P@ssw0rd"
   }
 
+  # Bogus set to link togeather resources for proper tear down
   set {
-    name  = "service.annotations.prometheus\\.io/port"
-    value = "9127"
-    type  = "string"
+    name  = "tf_link"
+    value = helm_release.cert_manager.name
   }
 }
+
+# resource "null_resource" "wait_for_rancher" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+# while [ "$${subject}" != "*  subject: CN=$${RANCHER_HOSTNAME}" ]; do
+#     subject=$(curl -vk -m 2 "https://$${RANCHER_HOSTNAME}/ping" 2>&1 | grep "subject:")
+#     echo "Cert Subject Response: $${subject}"
+#     if [ "$${subject}" != "*  subject: CN=$${RANCHER_HOSTNAME}" ]; then
+#       sleep 10
+#     fi
+# done
+# while [ "$${resp}" != "pong" ]; do
+#     resp=$(curl -sSk -m 2 "https://$${RANCHER_HOSTNAME}/ping")
+#     echo "Rancher Response: $${resp}"
+#     if [ "$${resp}" != "pong" ]; then
+#       sleep 10
+#     fi
+# done
+# EOF
+
+
+#     environment = {
+#       RANCHER_HOSTNAME = "${local.name}.${local.domain}"
+#       TF_LINK          = helm_release.rancher.name
+#     }
+#   }
+# }
--- environment/helm-install/provider.tf
+++ environment/helm-install/provider.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/helm"
       version = "2.10.1"
     }
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
   }
 }
 
@@ -11,5 +15,8 @@ provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
-
+}
+provider "kubectl" {
+  # Configuration options
+  config_path = "~/.kube/config"
 }
